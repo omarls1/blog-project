d8cb839911a5c3d6a9383a1a867ce4f9600c8622
omar lammas <omarlammas@gmail.com>
2024-07-27 15:15:06 +0200

    smoll and no important edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="ext-3xl font-bold underline">hello</div>
+  <div class="ext-3xl font-bold underline">
+    <router-link to="/dashboard">dashboard</router-link>
+  </div>
 </template>
